a59876f7a655608b9486bad7494d8d116448824c
confidenti-al <steve@confidenti.al>
2019-08-06 09:04:23 -0400

    Initial commit. Full CRUD on Companies and Technologies. Working on Search from Company API.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,145 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
+import CompanyIndex from './components/companyindex/CompanyIndex';
+import CompanyDetails from './components/companydetails/CompanyDetails';
+import Dashboard from './components/dashboard/Dashboard';
+import Signup from './components/signup/Signup';
+import Login from './components/login/Login';
+import AuthService from './services/AuthService';
+import Navbar from './components/navbar/Navbar';
+import Search from './components/search/Search';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = { 
+      listOfCompanies: [],
+      currentlyLoggedIn: null,
+      ready: false,
+      signupShowing: false,
+      loginShowing: false,
+  };
+  this.service = new AuthService();
+  }
+
+  getAllCompanies = () => {
+    axios.get(`http://localhost:5000/api/companies`, {withCredentials: true})
+    .then(responseFromApi => {
+      this.setState({
+        listOfCompanies: responseFromApi.data, ready: true
+      })
+    })
+  }
+
+  getCurrentlyLoggedInUser = () =>{
+    this.service.currentUser()
+    .then((theUser)=>{
+      this.setState({currentlyLoggedIn: theUser})
+    })
+    .catch(()=>{
+      this.setState({currentlyLoggedIn: null})
+    })
+  }
+
+  toggleForm = (whichForm) =>{
+    let theForm;
+    if(whichForm === "signup"){
+      theForm = 'signupShowing'
+    } else {
+      theForm = 'loginShowing'
+    }
+    this.setState({[theForm]: !this.state[theForm]})
+  }
+  
+  // componentWillMount() {
+  //   this.getAllCompanies();
+  //   this.getCurrentlyLoggedInUser();
+  // }
+
+  componentDidMount() {
+    this.getAllCompanies();
+    this.getCurrentlyLoggedInUser();
+  }
+
+
+  render(){
+
+    return (
+      <div>
+        <Navbar 
+        theUser = {this.state.currentlyLoggedIn} 
+        pleaseLogOut = {()=> this.service.logout()}
+        toggleForm = {this.toggleForm}
+        getUser = {this.getCurrentlyLoggedInUser}  
+        />
+
+      <Search />
+
+        {this.state.signupShowing && 
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-sm">
+              <Signup getUser={this.getCurrentlyLoggedInUser}
+              toggleForm={this.toggleForm} 
+              />
+              </div>
+            </div>
+          </div>
+        }
+
+        {this.state.loginShowing && 
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-sm">
+              <Login getUser={this.getCurrentlyLoggedInUser} toggleForm={this.toggleForm} />
+              </div>
+            </div>
+          </div>
+        }
+
+      <div className="container-fluid">
+        
+        <Switch>
+          <Route exact path="/" render ={(props)=> <CompanyIndex
+            {...props} 
+            theUser = {this.state.currentlyLoggedIn} 
+            allTheCompanies ={this.state.listOfCompanies}
+            getData = {this.getAllCompanies}
+            ready = {this.state.ready}
+            />} />
+
+          <Route exact path="/companies" render ={(props)=> <CompanyIndex
+            {...props} 
+            theUser = {this.state.currentlyLoggedIn} 
+            allTheCompanies ={this.state.listOfCompanies}
+            getData = {this.getAllCompanies}
+            ready = {this.state.ready}
+            />} />
+
+          <Route exact path="/dashboard" render ={(props)=> <Dashboard
+            {...props} 
+            theUser = {this.state.currentlyLoggedIn} 
+            allTheCompanies ={this.state.listOfCompanies}
+            getData = {this.getAllCompanies}
+            ready = {this.state.ready}
+            />} />
+
+          <Route exact path="/companies/:theID" render ={(props)=> <CompanyDetails
+            {...props} 
+            allTheCompanies ={this.state.listOfCompanies}
+            ready = {this.state.ready}
+            getData = {this.getAllCompanies}
+            theUser = {this.state.currentlyLoggedIn}
+            />} />
+
+
+        </Switch>
+      </div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/components/search/Search.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import axios from 'axios';
+
+class Search extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = { 
+      listOfCompanies: [],
+      searchField: '',
+      companyName: '',
+      companySiteUrl: '',
+      companyLogoUrl: '',       
+    };
+  }
+
+  getAllCompanies = () => {
+    axios.get(`http://localhost:5000/api/companies`)
+    .then(responseFromApi => {
+      this.setState({
+        listOfCompanies: responseFromApi.data, ready: true
+      })
+    })
+  }
+
+  onInputChange=(e)=>{
+    console.log(e.target.name, e.target.value)
+      const { name, value } = e.target;
+      this.setState({ [name]: value });
+      this.checkForCompany();
+    }
+
+    checkForCompany() {
+    const matchedCompany = this.state.listOfCompanies.filter(company => {
+      if (company.companyName.includes(this.state.searchField)) {
+        return true;
+      } else {
+        this.getData(this.value);
+        // console.log(this.value);
+        this.setState({
+          companyName: this.state.companyName,
+          companySiteUrl: this.state.companySiteUrl,
+          companyLogoUrl: this.state.companyLogoUrl,
+        });
+        // console.log(this.state)
+      }
+    })};
+
+  getData(name) {
+    // console.log(name)
+    axios.get('https://autocomplete.clearbit.com/v1/companies/suggest?query=:' + name)
+      .then((response) => {
+        console.log(response)
+        // return response.json();
+      })
+    }
+
+  componentWillMount(){
+    this.getAllCompanies();
+  }
+
+  render(){
+    return(
+    // not sure what to do with this stuff below
+    //companyName.text(data.name);
+    //companySiteUrl.text(data.domain);
+    //companyLogoUrl.attr('src', data.logo);
+    <div className="container">
+      <div className="input-group py-4">
+            <input className="form-control form-control-lg" type="search" id="example-search-input" name="searchField" value={this.state.searchField} onChange={(e)=>{this.onInputChange(e)}} placeholder="enter a company name..." />
+            <span className="input-group-append">
+              <button className="btn btn-outline-secondary disabled" type="button">
+                  <i className="fa fa-search"></i>
+              </button>
+            </span>
+      </div>
+    </div>
+
+
+    
+    )}
+}
+
+export default Search;
